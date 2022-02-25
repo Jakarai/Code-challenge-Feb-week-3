b42048ed552b44816ddb01f7f1c0b6bad5f0c6e3
Jakarai <56364114+Jakarai@users.noreply.github.com>
2022-02-25 16:44:07 -0500

    update

--- Java-chal.java
+++ Java-chal.java
@@ -1,7 +1,7 @@
 public class Main {
-   public static void main(String[] args) {
-
-      /* Java,C# & Python:
+  
+   
+    /* Java,C# & Python:
        Take the number 192 and multiply it by each of 1, 2, and 3:
        192 × 1 = 192
        192 × 2 = 384
@@ -10,10 +10,6 @@ public class Main {
        The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5,giving the pandigital, 918273645, which is the concatenated product of 9 and(1,2,3,4,5).
        What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n >1?
     }*/
-
-       System.out.println("The largest pandigital is "+pandigitalFind());
-
-    }
 
     public static long pandigitalFind() {
 
